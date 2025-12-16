dd0b9baf53f55be295a32040aa2a1a3fcd8609b9
SAMPATHIHARSHAVARDHAN <harshavardhansampathi@gmail.com>
2025-12-16 17:34:49 +0530

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,19 @@
+# Program to check Armstrong number
+
+num = int(input("Enter a number: "))
+
+# Convert number to string to find the number of digits
+n = len(str(num))
+
+sum_of_powers = 0
+temp = num
+
+while temp > 0:
+    digit = temp % 10
+    sum_of_powers += digit ** n
+    temp //= 10
+
+if num == sum_of_powers:
+    print(num, "is an Armstrong number")
+else:
+    print(num, "is not an Armstrong number")
