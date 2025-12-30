ce1c760f29d28ca39253997f4bd8817710ab982d
SAMPATHIHARSHAVARDHAN <harshavardhansampathi@gmail.com>
2025-12-30 19:09:12 +0530

    Add files via upload

---
+++ SingleDemo.java
@@ -0,0 +1,23 @@
+class A
+{
+A()
+{
+System.out.println("Inside A's Constructor");
+}
+}
+
+class B extends A
+{
+B()
+{
+System.out.println("Inside B's Constructor");
+}
+}
+
+class SingleDemo
+{
+public static void main(String args[])
+{
+B b1=new B();
+}
+}
